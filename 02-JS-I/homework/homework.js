6147b7763e95401d184117cd655a9fc2ccb3786a
Jhohan <mrcrock@outlook.es>
2022-10-13 09:49:35 -0500

    se completa homewor JS-I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -207,14 +207,14 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return "hola (nombre)!";
+  return "hola "+(nombre)+"!";
 
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  var area = alto * (ancho/2);
+  var area = alto * ancho;
 
   return area;
 }
@@ -223,6 +223,7 @@ function obtenerAreaRectangulo(alto, ancho) {
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
+  return lado * 4;
 
 }
 
@@ -230,7 +231,9 @@ function retornarPerimetro(lado){
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
+  var area = alto * (base/2);
 
+  return area;
 }
 
 
@@ -238,7 +241,8 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-
+  const dolar = 1.20;
+  return euro * dolar;
 }
 
 
@@ -248,7 +252,15 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-
+  if (letra.length > 1) {
+    return "Dato incorrecto";
+  }
+  else if (letra == "a" || letra=="e" || letra=="i" || letra=="o" || letra=="u") {
+    return "Es vocal";
+  }
+  else {
+    return "Dato incorrecto";
+  }
 }
 
 
