91a80a77e4e6cea011fa5df8b45240b387bb75d4
Jhohan <mrcrock@outlook.es>
2022-10-15 10:53:49 -0500

    se arreglan ejerciocios

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -49,7 +49,7 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
-  var array = array.unshift(elemento);
+  array.unshift(elemento);
   return array;
 }
 
