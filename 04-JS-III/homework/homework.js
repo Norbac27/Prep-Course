d54ff856a635bb291a0b4c55ba8d72bad010afdd
Jhohan <mrcrock@outlook.es>
2022-10-15 14:01:25 -0500

    se arreglan ejerciocios

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -148,7 +148,13 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  var conteo = 0;
+  for (var i = 0; i < arreglo.length; i++) {
+    if (arreglo[i]>18) {
+      conteo++
+    }
+  }
+  return conteo;
 }
 
 
@@ -157,7 +163,12 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario.
   //Escribe tu código aquí
-
+  if (numeroDeDia==1||numeroDeDia==7) {
+    return "Es fin de semana";
+  }
+  else {
+    return "Es dia laboral";
+  }
 }
 
 
