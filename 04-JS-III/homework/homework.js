80b0b8b8276b6af97c573f86b6d27441617a7ed8
Jhohan <mrcrock@outlook.es>
2022-10-15 11:17:16 -0500

    se arreglan ejerciocios

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -77,7 +77,7 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
-  if (array.include(elemento)) {
+  if (array.include(elemento)==true) {
     return true;
   }
   else {
