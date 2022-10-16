61d1060ef6ce268c3365f14516793351068e3fbd
Jhohan <mrcrock@outlook.es>
2022-10-15 19:30:24 -0500

    se arreglan ejerciocios

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -249,7 +249,23 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
-  
+  var array = [];
+  var suma = numero;
+  for (var i = 0; i < 10; i++) {
+    suma += 2;
+    if (suma==i) {
+      break
+    }
+    else {
+      array.push(suma);
+    }
+  }
+  if (i<10) {
+    return "Se interrumpió la ejecución";
+  }
+  else {
+    return array;
+  }
 }
 
 
