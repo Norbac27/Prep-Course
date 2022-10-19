9b08be21f07a9bcbc73b8d0a61ab177ed983937f
Jhohan <mrcrock@outlook.es>
2022-10-19 14:08:50 -0500

    se realiza punto 2 JSVI.js

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -19,7 +19,7 @@ function mayuscula(nombre) {
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
-  cb.callback();
+  return cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
