e762dc7facdebe61b3073161b367920b0e657967
Jhohan <mrcrock@outlook.es>
2022-10-19 10:46:28 -0500

    se realiza punto 4

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -36,7 +36,9 @@ function multiplicarNumeroDesconocidoPorCinco (objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
-  objetoMisterioso[numeroMisterioso]*5;
+
+  var result = objetoMisterioso.numeroMisterioso*5;
+  return result;
 }
 
 function eliminarPropiedad (objeto, unaPropiedad) {
