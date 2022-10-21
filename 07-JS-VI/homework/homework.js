a8575b3ff705da9dcce258fd6004796ee1475a79
Jhohan <mrcrock@outlook.es>
2022-10-21 17:36:29 -0500

    se realiza ejercicio 5 JSVI.test.js

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -45,8 +45,9 @@ function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
-  array.forEach(cb(item, i) => {
-  });
+  for (var i = 0; i < array.length; i++) {
+    return cb(array[i]);
+  }
 
 }
 
