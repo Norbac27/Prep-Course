b7d2f2e78922007c53c8d0ccfa4272d1aaf17ce5
Jhohan <mrcrock@outlook.es>
2022-10-21 17:59:15 -0500

    se realiza ejercicio 6 JSVI.test.js

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -67,6 +67,13 @@ function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i][0]=="a") {
+      newArray += array[i];
+    }
+  }
+  return newArray;
 
 }
 
