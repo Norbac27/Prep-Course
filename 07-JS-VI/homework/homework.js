d70d8ad1cf222b67408c3183c13494da11f05fcb
Jhohan <mrcrock@outlook.es>
2022-10-21 17:47:19 -0500

    se realiza ejercicio 5 JSVI.test.js

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -56,6 +56,10 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var newArray = array.map(function(el){
+    return cb(el);
+  });
+
 }
 
 function filter(array) {
